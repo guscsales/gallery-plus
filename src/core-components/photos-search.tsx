514fa50270b1eae04d7154c196366b64af6fc55c
Gustavo Sales <gus@codegus.com.br>
2025-06-22 17:09:49 -0300

    adds debounce for search

--- src/core-components/photos-search.tsx
+++ src/core-components/photos-search.tsx
@@ -2,9 +2,24 @@ import InputText from "../components/input-text";
 import SearchIcon from "../assets/icons/search.svg?react";
 import Divider from "../components/divider";
 import usePhotos from "../hooks/use-photos";
+import {debounce} from "../helpers/utils";
+import React from "react";
 
 export default function PhotosSearch() {
 	const {filters} = usePhotos();
+	const [inputValue, setInputValue] = React.useState(filters.q || "");
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const debouncedSetQ = React.useCallback(
+		debounce((value: string) => filters.setQ(value), 200),
+		[filters.setQ]
+	);
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setInputValue(value);
+		debouncedSetQ(value);
+	};
 
 	return (
 		<>
@@ -12,8 +27,8 @@ export default function PhotosSearch() {
 				icon={SearchIcon}
 				placeholder="Buscar fotos"
 				className="flex-1"
-				value={filters.q || ""}
-				onChange={(e) => filters.setQ(e.target.value)}
+				value={inputValue}
+				onChange={handleInputChange}
 			/>
 			<Divider orientation="vertical" className="h-10" />
 		</>
